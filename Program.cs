659d937f0ccb3c13af4c8fb1d9a1a3dc8cdea5c2
Hassan Mohamed <hassanalimohamed1@gmail.com>
2021-06-21 10:39:51 +1000

    First Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConvertMilesToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of miles: ");
+            int miles = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"{miles} miles in kilometres is {Change(miles)}.");
+        }
+        private static double Change(int miles) {
+            const double kilometre = 1.60934;
+            double convert = kilometre * Convert.ToDouble(miles);
+            return convert;
+        }
+    }
+}
